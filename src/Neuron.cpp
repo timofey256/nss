ebd75a5e84a13e8b6816c605b3e31d3350119a58
schetilt <shetilin.timofey@gmail.com>
2023-08-21 01:35:26 +0200

    Added #ifndef for Neuron class to resolve includings

--- src/Neuron.cpp
+++ src/Neuron.cpp
@@ -1,38 +1,20 @@
-#include <cmath>
+#ifndef NEURON_HPP
+#define NEURON_HPP
+
 #include <vector>
+#include <cmath>
 
 class Neuron {
 public:
-        Neuron(size_t index);
-        void setOutput(double val) { output = val; }
-        double getOutput() { return output; }
-        void feedForward();
-        void addConnection(Neuron neuron, double weight);
+    Neuron(size_t index);
+    void setOutput(double val);
+    double getOutput() const;
+    void feedForward();
+    void addConnection(Neuron neuron, double weight);
 private:
-        size_t myIndex;
-        double output;
-        std::vector<std::pair<Neuron, double>> connections; // connection with neurons from *previous* layers!
+    size_t myIndex;
+    double output;
+    std::vector<std::pair<Neuron, double>> connections;
 };
 
-typedef std::vector<Neuron> Layer;
-
-Neuron::Neuron(size_t index) {
-        myIndex = index;
-}
-
-void Neuron::addConnection(Neuron neuron, double weight) {
-        std::pair<Neuron, double> con = std::make_pair(neuron, weight);
-
-        connections.push_back(con);
-}
-
-void Neuron::feedForward() {
-        double sum = 0.0;
-
-        for (size_t n = 0; n < connections.size(); n++) {
-                sum += connections[n].first.getOutput() * connections[n].second;
-        }
-
-        output = tanh(sum);
-}
-
+#endif
