0370c762cb80e04f52a6f435ad79f6ef2d7b714a
schetilt <shetilin.timofey@gmail.com>
2023-08-23 23:40:35 +0200

    Added disappeared (???) implementation of Neuron

--- src/Neuron.cpp
+++ src/Neuron.cpp
@@ -1,6 +1,3 @@
-#ifndef NEURON_HPP
-#define NEURON_HPP
-
 #include <vector>
 #include <cmath>
 
@@ -17,4 +14,32 @@ private:
     std::vector<std::pair<Neuron, double>> connections;
 };
 
-#endif
+typedef std::vector<Neuron> Layer;
+
+Neuron::Neuron(size_t index) {
+        myIndex = index;
+}
+
+void Neuron::addConnection(Neuron neuron, double weight) {
+        std::pair<Neuron, double> con = std::make_pair(neuron, weight);
+
+        connections.push_back(con);
+}
+
+void Neuron::feedForward() {
+        double sum = 0.0;
+
+        for (size_t n = 0; n < connections.size(); n++) {
+                sum += connections[n].first.getOutput() * connections[n].second;
+        }
+
+        output = tanh(sum);
+}
+
+double Neuron::getOutput() const {
+	return output;
+}
+
+void Neuron::setOutput(double val) {
+	output = val;
+}
