87705a307901c4ad1aa199f08d3c4a523fd88d98
schetilt <shetilin.timofey@gmail.com>
2023-09-05 14:16:27 +0200

    Added genome generation

--- src/Nature.cpp
+++ src/Nature.cpp
@@ -1,6 +1,12 @@
+#include <array>
 #include "Nature.hpp"
 
-std::vector<Gene> Nature::generateRandomGenome(int length) {
-	std::vector<Gene> ans;
-	return ans;
+std::array<Gene, GENOME_LENGTH> Nature::generateRandomGenome(const int length) {
+	std::array<Gene, GENOME_LENGTH> genome;
+	for (int i=0; i<length; i++) {
+		Gene new_gene = {1,1,1,1};
+		genome[i] = new_gene;
+	}
+
+	return genome;
 }
--- src/Nature.hpp
+++ src/Nature.hpp
@@ -1,9 +1,20 @@
 #include <vector>
-#include "Gene.cpp"
+#include <cstddef>
+#include <cstdint>
+#include <array>
+
+constexpr int GENOME_LENGTH = 8;
+
+struct Gene {
+        uint8_t connectionLayerIndex;
+        uint8_t sourceNeuronIndex;
+        uint8_t destNeuronIndex;
+        uint8_t weight;
+};
 
 class Nature {
 public:
-	static std::vector<Gene> generateRandomGenome(int length);
+	static std::array<Gene, GENOME_LENGTH> generateRandomGenome(const int length);
 private:
 	Nature() {};
 };
