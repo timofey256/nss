3ccc38504f41f25b9da58aa7cc399a571dd5f437
schetilt <shetilin.timofey@gmail.com>
2023-09-19 18:58:44 +0200

    Added better time handling in the main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <chrono>
+#include <ctime>    
 #include <SFML/Graphics.hpp>
 #include "Grid.hpp"
 
@@ -9,6 +11,11 @@ constexpr int WINDOW_HEIGHT = 700;
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), APPLICATION_TITLE);
+	
+	auto now = std::chrono::system_clock::now().time_since_epoch();
+	auto last_in_millis = std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
+
+	int step_in_millis = 2000;
 
 	Grid grid;
 	while (window.isOpen()) {
@@ -17,12 +24,20 @@ int main() {
 			if (ev.type == sf::Event::Closed) {
 				window.close();
 			}
-
-			window.clear(sf::Color::Black);
-			grid.draw(&window);
-			grid.moveCells();	
-			window.display();
 		}
+
+		window.clear(sf::Color::Black);
+		grid.draw(&window);
+
+		auto n = std::chrono::system_clock::now().time_since_epoch();
+		auto cur = std::chrono::duration_cast<std::chrono::milliseconds>(n).count();
+		
+		if (cur-last_in_millis > step_in_millis) {
+			grid.moveCells();
+			last_in_millis = cur;	
+		}
+
+		window.display();
 	}
 
 	return 0;
